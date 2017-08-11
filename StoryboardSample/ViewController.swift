9f790d9b9ba43a334e1707c1695e5e309a3d079a
Hakbum Kim <rlagkrqj@nave.com>
2017-08-11 17:39:53 +0900

    코드로 화면전환 구현 완료.

--- StoryboardSample/ViewController.swift
+++ StoryboardSample/ViewController.swift
@@ -10,8 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func openAD(_ sender: Any) {
+    
+        
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "grayscene")
+        
+        if let newVc = vc {
+            
+            self.modalTransitionStyle = .flipHorizontal
+
+            self.present(newVc, animated: true, completion: nil)
+        }
+        
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
